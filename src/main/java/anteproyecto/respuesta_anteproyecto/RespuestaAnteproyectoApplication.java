99078abf36188ca1a0f82f1cdeae4eaad01b83f1
1754407938 <sav.mina@yavirac.edu.ec>
2023-01-30 20:09:33 -0500

    first commit

---
+++ src/main/java/anteproyecto/respuesta_anteproyecto/RespuestaAnteproyectoApplication.java
@@ -0,0 +1,13 @@
+package anteproyecto.respuesta_anteproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespuestaAnteproyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespuestaAnteproyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/anteproyecto/respuesta_anteproyecto/RespuestaAnteproyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package anteproyecto.respuesta_anteproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespuestaAnteproyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
